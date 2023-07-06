846f646f71f5706002597d7cc64e73faf614fe08
Muhammad Fuad Ardiono <fuadardiono16@gmail.com>
2023-07-06 12:39:32 +0800

    [Fuad] Improve cors

--- src/main/java/id/fuad/payment/config/DomainConfig.java
+++ src/main/java/id/fuad/payment/config/DomainConfig.java
@@ -1,9 +1,16 @@
 package id.fuad.payment.config;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 
 @Configuration
@@ -11,4 +18,16 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan("id.fuad.payment")
 @EnableJpaRepositories("id.fuad.payment")
 public class DomainConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        // Don't do this in production, use a proper list  of allowed origins
+        config.setAllowedOrigins(Collections.singletonList("http://localhost:5173"));
+        config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
 }
